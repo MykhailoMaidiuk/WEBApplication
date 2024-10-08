6d683631633fa3bdb111c9ae4232243dd501571e
mykhailo.maidiuk <mykhailo.maidiuk@tul.cz>
2024-10-08 14:47:55 +0200

    change ports in front and backend

--- backend/app.py
+++ backend/app.py
@@ -5,19 +5,19 @@ import os
 
 app = Flask(__name__)
 
-# Настройка логирования
+# logs
 log_directory = os.path.join(os.getcwd(), 'backend/logs')
 os.makedirs(log_directory, exist_ok=True)
 logging.basicConfig(
-    filename=os.path.join(log_directory, 'app.log'),  # Исправлено
+    filename=os.path.join(log_directory, 'app.log'),  # fixed
     level=logging.INFO,
     format='%(asctime)s %(levelname)s %(message)s'
 )
-
+#app.log a error.log
 @app.route("/")
 def hello_world():
     app.logger.info("HelloWorld endpoint was called")
     return "Hello, World!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8009)  # Обновите хост и порт
